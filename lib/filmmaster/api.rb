4c54ed1f59fb6c9de68ea480cd012a87191e68f8
kelly r maass <kmaass77627@gmail.com>
2019-12-10 01:31:58 +0000

    cli

--- lib/filmmaster/api.rb
+++ lib/filmmaster/api.rb
@@ -6,7 +6,6 @@ class Filmmaster::API
   
    def fetch 
     key = ENV["efe7f0794a75203b20e8516137dc9c30"]
-    
     url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=efe7f0794a75203b20e8516137dc9c30&language=en-US&page=1'
     response = HTTParty.get(url)
     response["results"].each do |movie|
@@ -16,7 +15,7 @@ class Filmmaster::API
     overview = movie["overview"]
     Filmmaster::Movie.new(name, popularity, vote_average, overview)
   end 
-  
+end 
  
   
 end
--- lib/filmmaster/cli.rb
+++ lib/filmmaster/cli.rb
@@ -3,9 +3,19 @@ class Filmmaster::CLI
   
   def call 
    puts "Welcome to Filmmaster!!"
+   "Here are the current movies playing near you!"
    Filmmaster::API.new.fetch
-   
+   menu 
+   goodbye
+    end 
+  
+  def menu 
+    ""
+    puts "Enter the name of the movie you would like more information on"
   end 
   
+  def goodbye
+    "Thank you for using Filmmaster!! Enjoy your Movie!"
+    
   
 end
