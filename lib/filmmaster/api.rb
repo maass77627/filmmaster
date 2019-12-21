17f432db18654c9619ebf1fc7467055bc3c70ce4
kelly r maass <kmaass77627@gmail.com>
2019-12-21 06:30:27 +0000

    c

--- lib/filmmaster/api.rb
+++ lib/filmmaster/api.rb
@@ -5,16 +5,16 @@ require 'httparty'
 class Filmmaster::API
   
   def fetch
-     url = URI.parse "https://ghibliapi.herokuapp.com/films"
+    url = URI.parse "https://ghibliapi.herokuapp.com/films"
     response = Net::HTTP.get(url)
     results = JSON.parse response 
     results.each do |movie|
-      name = movie["title"]
-      director = movie["director"]
-      description = movie["description"]
-      Filmmaster::Movie.new(name, director, description)
+    name = movie["title"]
+    director = movie["director"]
+    description = movie["description"]
+    Filmmaster::Movie.new(name, director, description)
     end 
-end 
+  end 
  
   
 end
