69f264cc87f3a61500fe77dbd9d81141e4cee8c5
kelly r maass <kmaass77627@gmail.com>
2019-12-21 06:39:50 +0000

    changes

--- lib/filmmaster/cli.rb
+++ lib/filmmaster/cli.rb
@@ -10,26 +10,26 @@ class Filmmaster::CLI
 
   def list_movies 
     puts "Welcome to Filmmaster!,Here are the top Studio Ghibli films!"
-    Filmmaster::API.new.fetch
-    Filmmaster::Movie.all.each_with_index do |movie, index|
+      Filmmaster::API.new.fetch
+      Filmmaster::Movie.all.each_with_index do |movie, index|
     puts "#{index+1}. #{movie.name}" 
     end 
   end 
 
   def menu 
-    input = nil
+      input = nil
     while input != "exit"
-    puts "Enter the number of the movie you would like more information on"
-    input = gets.strip
+       puts "Enter the number of the movie you would like more information on"
+       input = gets.strip
     if input.to_i > 0 && input.to_i < Filmmaster::Movie.all.length 
-    chosen_movie = Filmmaster::Movie.all[input.to_i-1]
-    puts chosen_movie.name 
-    puts chosen_movie.director
-    puts chosen_movie.description
+       chosen_movie = Filmmaster::Movie.all[input.to_i-1]
+       puts chosen_movie.name 
+       puts chosen_movie.director
+       puts chosen_movie.description
 	  elsif input == "list"
-	  list_movies
+	     list_movies
 	  else
-	  puts "Not sure what you want, type list or exit."
+	    puts "Not sure what you want, type list or exit."
 	  end
 	  end
  end
